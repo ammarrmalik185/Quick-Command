455219e4b0b8f92ece1b3498e8ce4de96d87df0f
Ammar Malik <ammarrmalik185@hotmail.com>
2025-12-03 17:13:35 +0500

    fix: remove built in commands and update qc script

--- default_commands.ps1
+++ default_commands.ps1
@@ -14,4 +14,33 @@ $default_commands = @{
         }
     }
 
+    help = @{
+        Title = "List all commands"
+        Command = {
+            Write-Host "`nAvailable commands:`n"
+            foreach ($name in $commands.Keys) {
+                Write-Host ("  {0,-12} - {1}" -f $name, $commands[$name].Title)
+            }
+            Write-Host "`nDefault commands:`n"
+            foreach ($name in $default_commands.Keys) {
+                Write-Host ("  {0,-12} - {1}" -f $name, $default_commands[$name].Title)
+            }
+            Write-Host ("")
+        }
+    }
+
+    edit = @{
+        Title = "Edit the commands file"
+        Command = {
+            code $commandFilePath
+        }
+    }
+
+    qc = @{
+        Title = "Go to quick command directory"
+        Command = {
+            Set-Location $PSScriptRoot
+        }
+    }
+
 }
--- qc.ps1
+++ qc.ps1
@@ -18,41 +18,7 @@ if (-not (Test-Path $commandFilePath)) {
 . $commandFilePath
 . $defaultCommandFilePath
 
-# Built-in commands
-$builtin = @{
-    help = @{
-        Title = "List all commands"
-        Command = {
-            Write-Host "`nAvailable commands:`n"
-            foreach ($name in $commands.Keys) {
-                Write-Host ("  {0,-12} - {1}" -f $name, $commands[$name].Title)
-            }
-            Write-Host "`nDefault commands:`n"
-            foreach ($name in $default_commands.Keys) {
-                Write-Host ("  {0,-12} - {1}" -f $name, $default_commands[$name].Title)
-            }
-            Write-Host "`nBuilt-in commands:`n"
-            foreach ($name in $builtin.Keys) {
-                Write-Host ("  {0,-12} - {1}" -f $name, $builtin[$name].Title)
-            }
-            Write-Host ("")
-        }
-    }
-    edit = @{
-        Title = "Edit the commands file"
-        Command = {
-            code $commandFilePath
-        }
-    }
-    qc = @{
-        Title = "Go to quick command directory"
-        Command = {
-            Set-Path $PSScriptRoot
-        }
-    }
-}
-
-$all = $commands + $builtin + $default_commands
+$all = $commands + $default_commands
 
 # Run command
 if ($all.ContainsKey($commandName)) {
