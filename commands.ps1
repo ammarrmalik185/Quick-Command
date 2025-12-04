c14eeb861ea7edeaf79b1cd7bc802149d473953f
Ammar Malik <51779977+ammarrmalik185@users.noreply.github.com>
2025-12-04 10:28:39 +0500

    Refactor commands in commands.ps1 for clarity

--- commands.ps1
+++ commands.ps1
@@ -1,45 +1,17 @@
 $commands = @{
-    kt = @{
-        Title = "Run KT Connector"
+    # Example: Navigate to a project
+    myproj = @{
+        Title = "Go to My Project"
         Command = {
-            cd 'C:/software/kt connect'
-            .\ktctl.exe connect --kubeconfig .\kube-new.config --debug --namespace dev
+            Set-Location 'C:\Projects\MyProject'
         }
     }
 
-    mda = @{
-        Title = "Run ZCAD Frontend"
+    # Example: Run a build script
+    build = @{
+        Title = "Run Build"
         Command = {
-            cd 'D:/projects/zixel/zcad/mda'
-            pnpm run dev:pc
-        }
-    }
-
-    ivy = @{
-        Title = "Go to IVY Project directory"
-        Command = {
-            cd 'D:/projects/zixel/zcad/ivyproject'
-        }
-    }
-
-    ivy2 = @{
-        Title = "Go to IVY Project 2 directory"
-        Command = {
-            cd 'D:/projects/zixel/zcad/ivyproject_2'
-        }
-    }
-
-    conan = @{
-        Title = "Run Conan Install"
-        Command = {
-            conan install . --output-folder=build --build='*' --settings=build_type=Debug -r cpp-cad-group
-        }
-    }
-
-    conan_m = @{
-        Title = "Run Conan Install Missing"
-        Command = {
-            conan install . --output-folder=build --build=missing --settings=build_type=Debug -r cpp-cad-group
+            npm run build
         }
     }
 }
